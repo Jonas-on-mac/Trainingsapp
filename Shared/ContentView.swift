7170e253c67a9491579fdf77e6a391c24d03ab93
Jonas-on-mac <79999362+Jonas-on-mac@users.noreply.github.com>
2021-03-03 21:15:51 +0100

    Start der app

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Jonas hat einen kleinen Penis und Enrico hat den größten, jeder weis das schaben den längsten haben und indianer am längsten durchhalten, darf ich mich vorstellen, mein name winetoo häberle")
-            .padding()
+    
+            Text("Hello world!")
+                .padding()
+        
+        
+        
     }
 }
 
@@ -19,6 +23,9 @@ struct ContentView: View {
 //Vorschau
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        
+        }
     }
 }
