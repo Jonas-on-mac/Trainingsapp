247fe34c9a209928167e8f29b212e9d4d655db36
Jonas-on-mac <79999362+Jonas-on-mac@users.noreply.github.com>
2021-03-03 20:41:36 +0100

    Initial Commit

---
+++ Shared/TrainingsappApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingsappApp.swift
+//  Shared
+//
+//  Created by Jonas Pramann on 03.03.21.
+//
+
+import SwiftUI
+
+@main
+struct TrainingsappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
