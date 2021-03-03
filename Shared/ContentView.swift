cfe1b6e453c6ab18a49c6b05e123903555b9d363
Jonas-on-mac <79999362+Jonas-on-mac@users.noreply.github.com>
2021-03-03 20:49:29 +0100

    change test

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,11 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, World!")
             .padding()
     }
 }
 
+
+
+//Vorschau
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
