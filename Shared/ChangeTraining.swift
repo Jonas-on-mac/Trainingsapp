c99ee23a27f9fbb2ebb1681b2c207554066444d6
Jonas-on-mac <79999362+Jonas-on-mac@users.noreply.github.com>
2021-03-04 20:19:36 +0100

    training erstellen

--- Shared/ChangeTraining.swift
+++ Shared/ChangeTraining.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct ChangeTraining: View {
+    @State private var steppervalue = 0
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        ZStack {    //HINTERGRUND
+            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple
+            ]), startPoint: .top, endPoint: .bottom)
+            .ignoresSafeArea()
+          
+            
+            
+           
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            Stepper(value: $steppervalue, in: 0...15) {
+                Text("Add\(steppervalue > 0 ? "Übung ": " ")")
+                
+                
+            }
+            
+        }
     }
 }
 
+struct choosesport: View {
+    var body: some View{
+        
+      Text("")
+        
+        
+    }
+}
+
+
+
+
+
+
+//Vorschau
 struct ChangeTraining_Previews: PreviewProvider {
     static var previews: some View {
         ChangeTraining()
